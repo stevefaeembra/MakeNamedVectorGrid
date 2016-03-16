4318ac9ff1d0e99a8acdcfa768e434ca28d70a0e
stevefaeembra <stevendkay@gmail.com>
2016-03-16 14:56:04 +0000

    fixed spinbox issue in windows

--- resources_rc.py
+++ resources_rc.py
@@ -2,7 +2,7 @@
 
 # Resource object code
 #
-# Created: Wed Mar 16 14:33:31 2016
+# Created: Wed Mar 16 14:55:39 2016
 #      by: The Resource Compiler for PyQt (Qt v4.8.6)
 #
 # WARNING! All changes made in this file will be lost!
